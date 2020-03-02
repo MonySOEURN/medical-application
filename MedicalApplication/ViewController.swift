2954647514d8feefe7e404038e5eeefa48728c18
Mony SOEURN <soeurn.mony@gmail.com>
2020-03-02 10:08:20 +0700

    Initial Commit

---
+++ MedicalApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MedicalApplication
+//
+//  Created by Mony SOEURN on 3/2/2563 BE.
+//  Copyright © 2563 BE Mony SOEURN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
